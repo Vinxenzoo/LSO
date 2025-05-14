719879885334cb594698dd15617be8a37f132e6e
kali <duallovercouple@gmail.com>
2025-05-14 11:50:56 +0200

    Aggiunti metodi e variabili server commit -messages.h --Aggiunto file con i messaggi inviati dal server
    --datastructure.h
    --Aggiunta enum dei possibili esiti della partita
    
    -server.h
    --Aggiunte variabili per l'utilizzo degli struct
    
    -server.c
    --Aggiunti metodi

--- server/messages.h
+++ server/messages.h
@@ -1,9 +1,17 @@
 #ifndef MESSAGES_H
 #define MESSAGES_H
 
-#define VICTORY "1A"
-#define LOST    "1B"
-#define DRAW    "1C"
+#define SERVER_ERROR "01\n"
 
+#define VICTORY "1A\n"
+#define LOST    "1B\n"
+#define DRAW    "1C\n"
+
+
+#define INSERT_PLAYER_NAME "20\n"
+#define INVALID_PLAYER_NAME "21\n"
+#define ALREADY_TAKEN_NAME "22\n"
+
+#define REGISTRATION_COMPLETED "23\n"
 
 #endif
--- server/server.c
+++ server/server.c
@@ -37,3 +37,130 @@ Server server_init()
    return newServer;
 
 }
+
+void* player_thread( void *sd )
+{
+   const int player_sd = *( ( int * ) sd );
+   struct PlayerNode *player = register_player( player_sd );
+
+}
+
+
+struct PlayerNode* register_player( const int player_sd )
+{
+   char *player_name = check_player( player_sd );
+   struct PlayerNode *player = add_player( player_name );
+   free( player_name );
+   return player; 
+}
+
+char* check_player( const int player_sd )
+{
+   char *player_name = ( char * ) malloc( MAX_NAME_PLAYER * sizeof( char ) );
+
+   if( player_name == NULL)
+   {
+      send( player_sd, INVALID_PLAYER_NAME, 3, MSG_NOSIGNAL );
+      close( player_sd );
+      pthread_exit( NULL );
+   }
+
+   if( send( player_sd, INSERT_PLAYER_NAME, 3, MSG_NOSIGNAL ) < 0 )
+   {
+      close( player_sd );
+      free( player_name );
+      pthread_exit( NULL );
+   }
+
+   bool name_found = false;
+
+   do
+   {
+      memset( player_name, 0, MAX_NAME_PLAYER);
+
+      if( recv( player_sd, player_name, MAX_NAME_PLAYER, 0 ) <= 0 )
+      {
+         close( player_sd );
+         free( player_sd );
+         printf( "Check Error.\n" );
+         pthread_exit( NULL );
+      }
+
+      if( !player_found( player_name ) )
+      {
+         name_found = true;
+      }
+      else if( send( player_sd, ALREADY_TAKEN_NAME, 3, MSG_NOSIGNAL) < 0 )
+      {
+         close( player_sd );
+         free( player_name );
+         printf( "Name already taken.\n" );
+         pthread_exit( NULL );
+      }
+   }while( !name_found );
+
+   if( send( player_sd, REGISTRATION_COMPLETED, 3, MSG_NOSIGNAL ) < 0 )
+   {
+      close( player_sd );
+      free( player_name );
+      printf( "Check failed.\n" );
+      pthread_exit( NULL );
+   }
+
+   return player_name;
+}
+
+bool player_found( const char *player_name )
+{
+   pthread_mutex_lock( &player_mutex );
+   struct PlayerNode *player = player_head;
+
+   while( player != NULL )
+   {
+      if( strcmp( player->name, player_name ) == 0 )
+      {
+         pthread_mutex_unlock( &player_mutex );
+         return true;
+      }
+
+      player = player->next_node;
+
+   }
+
+   pthread_mutex_unlock( &player_mutex );
+   return false;
+
+}
+
+struct PlayerNode* add_player( const char *player_name, const int player_sd )
+{
+   struct PlayerNode *player = ( struct PlayerNode * ) malloc( sizeof ( struct PlayerNode ) );
+
+   if( player == NULL )
+   {
+      send( player_sd, SERVER_ERROR, 3, MSG_NOSIGNAL );
+      close( player_sd );
+      printf( "Memory Allocation Failed.\n" );
+      pthread_exit( NULL );
+   }
+
+   memset( player, 0, sizeof( struct PlayerNode ) );
+   strcpy( player->name, player_name );
+   player->wins = 0;
+   player->losts = 0;
+   player->draws = 0;
+   pthread_mutex_init( &( player->mutex_state ), NULL );
+   pthread_cond_init( &( player->cv_state ), NULL );
+   player->status = IN_LOBBY;
+   player->player_id = pthread_self();
+   player->player_sd = player_sd;
+   player->champion = false;
+
+   pthread_mutex_lock( &player_mutex );
+   player->next_node = player_head;
+
+   player_head = player;
+   pthread_mutex_unlock( &player_mutex );
+
+   return player;
+}
